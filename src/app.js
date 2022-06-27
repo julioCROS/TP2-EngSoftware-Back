439065533b893def6e1696b978a2a4a27dbebc01
Victor Le Roy Matos <victorlrmatos@gmail.com>
2022-06-27 12:16:31 -0300

    [+] CORS

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ dotenv.config();
 const app = express();
 const router = express.Router();
 app.use(express.json());
+app.use(function (req, res, next) { 
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token"); 
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); 
+    next(); 
+});
 
 //Conexão com o banco de dados (MongoDB)
 mongoose.connect(process.env.DB_CONNECTION.toString());
